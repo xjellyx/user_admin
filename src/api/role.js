4793cd0aedfebb825edf403a761ca432fb702d58
olongfen <olongfen@localhost.localdomain>
2020-09-08 17:52:22 +0800

    role

--- src/api/role.js
+++ src/api/role.js
@@ -6,3 +6,33 @@ export function getRoleList() {
         method:"get"
     })
 }
+
+export function getRoleLevelList() {
+    return request({
+        url:"/admin/getRoleLevel",
+        method:"get"
+    })
+}
+export function editRole(data) {
+    return request({
+        url:"/admin/editRole",
+        method:"put",
+        data
+    })
+}
+
+export function removeRole(id) {
+    return request({
+        url:"/admin/removeRole",
+        method:"delete",
+        params:{id:id}
+    })
+}
+
+export function addRole(data) {
+    return request({
+        url:"/admin/addRole",
+        method:"post",
+        data
+    })
+}
--- src/views/permission/role/index.vue
+++ src/views/permission/role/index.vue
@@ -14,14 +14,30 @@
             <el-table-column  fixed="right" align="center" label="Edit" width="400px" >
                 <template slot-scope="scope">
                     <el-button @click="openRoleDrawer(scope.row)" type="primary" size="small">Set Permission</el-button>
-                    <el-button @click="handleEditRole" icon="el-icon-edit" size="small" type="primary">Edit</el-button>
-                    <el-button icon="el-icon-delete" size="small" type="danger">Delete</el-button>
+                    <el-button @click="handleEditRole(scope.row)" icon="el-icon-edit" size="small" type="primary">Edit</el-button>
+                    <el-button @click="deleteRole(scope.row)" icon="el-icon-delete" size="small" type="danger">Delete</el-button>
                 </template>
             </el-table-column>
         </el-table>
 
         <el-dialog :before-close="handleCloseRoleDialog" :title="roleDialogTitle"
                    :visible.sync="dialogRoleVisible" center >
+          <el-form :model="roleForm"
+                   :rules="roleFormRules"
+                   ref="roleFormRefs"
+
+          >
+            <el-form-item label="Role"  prop="role">
+              <el-input placeholder="role" autocomplete="off" v-model="roleForm.role"></el-input>
+            </el-form-item>
+            <el-cascader
+                v-model="value"
+                :options="roleLevelOptions"
+                @change="changeRoleLevel"
+                filterable
+            >
+            </el-cascader>
+          </el-form>
           <div class="dialog-footer" slot="footer">
             <el-button @click="closeRoleDialog">Close</el-button>
             <el-button @click="enterRoleDialog" type="primary">Enter</el-button>
@@ -39,7 +55,7 @@
 </template>
 
 <script>
-    import {getRoleList} from "@/api/role";
+    import {getRoleList,getRoleLevelList,editRole,removeRole,addRole} from "@/api/role";
     import Apis from './components/apis'
 
     export default {
@@ -48,8 +64,22 @@
         data(){
             return {
                 activeRow: {},
-                addRoleForm:{},
+              value:[],
+                roleForm:{
+                  role:'',
+                  level:0,
+                  id:0,
+                },
                 roleData:[],
+                roleLevelOptions:[],
+                roleFormRules:{
+                    role: [
+                      { required: true, message: "role name", trigger: "blur" }
+                    ],
+                    level: [
+                      { required: true, message: "role level", trigger: "blur" }
+                    ]
+              },
                 dialogRoleVisible: false,
                 roleDrawer:false,
                 roleDialogTitle: '',
@@ -57,9 +87,24 @@
             }
         },
         created() {
-            this.handlerGetRoleList()
+          this.handlerGetRoleList()
+          this.setRoleLevelOptions()
+
         },
         methods: {
+          // set role level options
+            async setRoleLevelOptions(){
+              const {data} = await getRoleLevelList()
+              this.roleLevelOptions = []
+               data && data.forEach(item =>{
+                 let _d = {}
+                 _d["label"] = "Level "+item
+                 _d['value'] =  String(item)
+                 this.roleLevelOptions.push(_d)
+               })
+            },
+
+          // get role list
             async handlerGetRoleList (){
                const res= await getRoleList()
                 this.roleData = res.data
@@ -68,21 +113,98 @@
                 this.dialogRoleVisible = false
                 this.roleDialogTitle= ""
             },
+
+          // add
             handleAddRole(){
                 this.dialogRoleVisible = true
                 this.roleDialogTitle= "Add Role"
             },
-            handleEditRole(){
+
+          // edit
+            handleEditRole(row){
                 this.dialogRoleVisible = true
                 this.roleDialogTitle= "Edit Role"
+                this.roleForm = row
+                this.value = []
+                this.value.push(row.level)
             },
+
+          // delete
+          deleteRole(row){
+            this.$confirm('This operation will permanently delete all data, Whether to continue?', 'prompt', {
+              confirmButtonText: 'Yes',
+              cancelButtonText: 'No',
+              type: 'warning'
+            })
+                .then( async () => {
+                  const res = await removeRole(row.id)
+                  if (res) {
+                    this.$message({
+                      type: 'success',
+                      message: 'success!'})
+                  }
+                 await this.handlerGetRoleList()
+                })
+                .catch(() => {
+                  this.$message({
+                    type: 'info',
+                    message: 'Has been canceled'
+                  })
+                })
+          },
+
+          // open drawer
           openRoleDrawer(row){
             this.roleDrawer = true
             this.activeRow = row;
 
           },
-          closeRoleDialog(){},
-          enterRoleDialog(){},
+
+          // get value arr to level
+          changeRoleLevel(){
+              this.roleForm.level=this.value[0]
+          },
+
+          // cancel
+          closeRoleDialog(){
+            this.dialogRoleVisible = false
+            this.roleDialogTitle= ""
+            this.roleForm = {}
+            this.value = []
+          },
+
+          // enter
+          enterRoleDialog(){
+            this.$refs.roleFormRefs.validate(
+                async valid =>{
+                  if (valid){
+                    switch (this.roleDialogTitle){
+                      case "Add Role":
+                        await addRole(this.roleForm)
+                        break
+                      case "Edit Role":
+                        await editRole(this.roleForm)
+                        break
+                      default:
+                        return
+                    }
+                    this.$message({
+                      message:"success",
+                      type:"success",
+                    })
+                  }else {
+                    this.$message({
+                      message:"please input data",
+                      type:"waring",
+                    })
+                  }
+                }
+            )
+            this.dialogRoleVisible = false
+            this.roleDialogTitle= ""
+            this.roleForm = {}
+            this.value = []
+          },
         }
     }
 </script>

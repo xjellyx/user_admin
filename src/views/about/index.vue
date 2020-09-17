c6de9025002e930f7ecac559ba90f167e4f53b0b
olongfen <olongfen@localhost.localdomain>
2020-09-17 18:00:00 +0800

    add: about

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -1,15 +1,212 @@
 <template>
-    <div>
-        <h1>Todo</h1>
-    </div>
+  <div>
+    <el-row :gutter="10">
+      <el-col :span="12">
+        <el-card>
+          <div style="font-size: 25px" slot="header">Gorm-Gin-Admin</div>
+          <div>
+            <el-row>
+              <span style="font-size: 25px " >Welcome to star or fork</span>
+              <el-col :span="8" :offset="8">
+                <a href="https://github.com/olongfen/gorm-gin-admin">
+                  <img
+                      class="org-img dom-center"
+                      src="@/assets/logo.png"
+                      alt="gorm-gin-admin"
+                  />
+                </a>
+              </el-col>
+            </el-row>
+            <el-row :gutter="10">
+              <el-col :span="8">
+                <a href="https://github.com/olongfen/gorm-gin-admin">
+                  <img
+                      class="dom-center"
+                      src="https://img.shields.io/github/watchers/olongfen/gorm-gin-admin.svg?label=Watch"
+                      alt=""
+                  />
+                </a>
+              </el-col>
+              <el-col :span="8">
+                <a href="https://github.com/olongfen/gorm-gin-admin">
+                  <img
+                      class="dom-center"
+                      src="https://img.shields.io/github/stars/olongfen/gorm-gin-admin.svg?style=social"
+                      alt=""/>
+                </a
+                ></el-col>
+              <el-col :span="8">
+                <a href="https://github.com/olongfen/gorm-gin-admin">
+                  <img
+                      class="dom-center"
+                      src="https://img.shields.io/github/forks/olongfen/gorm-gin-admin.svg?label=Fork"
+                      alt=""/></a
+                ></el-col>
+            </el-row>
+          </div>
+        </el-card>
+        <el-card style="margin-top: 20px">
+          <div style="font-size: 25px" slot="header">Olongfen Github</div>
+          <div>
+            <el-row>
+              <el-col :span="8" :offset="8">
+                <a  href="https://github.com/olongfen">
+                  <img
+                      class="org-img dom-center"
+                      src="@/assets/xiaogui.png"
+                      alt="Olongfen"
+                  />
+                </a>
+              </el-col>
+            </el-row>
+<!--            <el-row style="margin-left: 40px" :gutter="20">-->
+<!--              <template v-for="(item, index) in members">-->
+<!--                <el-col :span="8" :key="index">-->
+<!--                  <a :href="item.html_url">-->
+<!--                    <img class="avatar-img" :src="item.avatar_url" />-->
+<!--                    <a class="author-name" style="">{{ item.login }}</a>-->
+<!--                  </a>-->
+<!--                </el-col>-->
+<!--              </template>-->
+<!--            </el-row>-->
+          </div>
+        </el-card>
+        <el-card style="margin-top: 20px">
+          <div style="font-size: 25px" slot="header">Olongfen Blog</div>
+          <div>
+            <el-radio v-model="radio" @change="radioChange" label="國內博客地址"></el-radio>
+            <el-radio v-model="radio" @change="radioChange" label="github.io"></el-radio>
+          </div>
+          <el-row style="margin-top: 8px">
+            <el-col :span="8" :offset="8">
+              <a :href="blogHref">
+                <img
+                    class="org-img dom-center"
+                    src="@/assets/xingui.png"
+                    alt="Olongfen"
+                />
+              </a>
+            </el-col>
+          </el-row>
+        </el-card>
+      </el-col>
+      <el-col :span="12">
+        <el-card>
+          <div slot="header">
+            Commit History
+          </div>
+          <div>
+            <Timeline
+                :timeline-items="dataTimeline"
+                :message-when-no-items="messageWhenNoItems"
+                :uniqueTimeline="true"
+                :unique-year="true"
+                :show-day-and-month="true"
+                order="desc"
+                dateLocale="en"
+            />
+          </div>
+          <el-button class="load-more" @click="loadMore" type="text"
+          >Load more</el-button
+          >
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "About"
+import { Commits, Members } from "@/api/github";
+import Timeline from "timeline-vuejs";
+export default {
+  name: "Test",
+  components: {
+    Timeline,
+  },
+  data() {
+    return {
+      messageWhenNoItems: "There arent commits",
+      members: [],
+      dataTimeline: [],
+      page: 0,
+      radio: '國內博客地址',
+      blogHref:'http://www.olongfen.ltd/blog/#/',
+
+    };
+  },
+  created() {
+    this.loadCommits();
+    this.loadMembers();
+  },
+  methods: {
+    loadMore() {
+      this.page++;
+      this.loadCommits();
+    },
+    loadCommits() {
+      Commits(this.page).then(({ data }) => {
+        data.forEach((element) => {
+          if (element.commit.message) {
+            this.dataTimeline.push({
+              from: new Date(element.commit.author.date),
+              title: element.commit.author.name,
+              showDayAndMonth: true,
+              description: `<a style="color: #26191b" href="${element.html_url}">${element.commit.message}</a>`,
+            });
+          }
+        });
+      });
+    },
+    loadMembers() {
+      Members().then(({ data }) => {
+        this.members = data;
+        this.members.sort();
+      });
+    },
+    radioChange(){
+      if (this.radio ==='github.io'){
+        this.blogHref = "https://olongfen.github.io"
+      }else {
+
+      }
+
     }
+  },
+};
 </script>
 
 <style scoped>
+.load-more {
+  margin-left: 120px;
+}
 
+.avatar-img {
+  float: left;
+  height: 40px;
+  width: 40px;
+  border-radius: 50%;
+  -webkit-border-radius: 50%;
+  -moz-border-radius: 50%;
+  margin-top: 15px;
+}
+
+.org-img {
+  height: 150px;
+  width: 150px;
+}
+
+.author-name {
+  float: left;
+  line-height: 65px !important;
+  margin-left: 10px;
+  color: darkblue;
+  line-height: 100px;
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+  "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+}
+
+.dom-center {
+  margin-left: 50%;
+  transform: translateX(-50%);
+}
 </style>
